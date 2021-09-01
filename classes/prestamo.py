a2548551c2944630eba963cb4db4352992379d4b
Juan Andrade <jpas.juan01@gmail.com>
2021-09-01 16:48:34 -0500

    Metodos de las clase Prestamo.

--- classes/prestamo.py
+++ classes/prestamo.py
@@ -10,3 +10,33 @@ class Prestamo:
         self.__estado = self.__calcular_estado(self.__saldo)
         self.empleado = emp
         self.__auto_incrementoId += 1
+
+    def __str__(self) -> str:
+        prestamo_cadena = '(' + self.__id + ', ' + self.fecha + ', ' + str(self.valor) + ', ' + str(self.numero_pagos)\
+                              + ', ' + str(self.cuota) + ', ' + str(self.__saldo) + ', ' + str(self.__estado) + ')'
+        return prestamo_cadena
+    
+    def __calcular_saldo(self, valor, numero_pagos, cuota) -> float:
+        if numero_pagos != 0:
+            saldo = round(((numero_pagos * cuota) - valor), 2)
+        else:
+            saldo = round((cuota - valor), 2)
+        return saldo
+
+    def __calcular_estado(self, saldo) -> bool:
+        if saldo != 0:
+            estado = True
+        else:
+            estado = False
+        return estado
+
+    def mostrar_empleado(self) -> None:
+        print('-Clase Prestamo:')
+        print(' id:', self.__id)
+        print(' fecha:', self.fecha)
+        print(' valor:', self.valor)
+        print(' numero_pagos:', self.numero_pagos)
+        print(' cuota:', self.cuota)
+        print(' saldo:', self.__saldo)
+        print(' estado:', self.__estado)
+        print(' Empleado():', self.empleado)
