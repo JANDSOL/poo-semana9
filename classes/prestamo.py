3d859faacb318f61215d4b2f47ec4c89bc6e53a7
Juan Andrade <jpas.juan01@gmail.com>
2021-09-01 18:08:20 -0500

    Metodo de la clase PagoNomina + correccion nombre metodo PagoSobretiempo + correccion nombre metodo Prestamo + mecanismo nuevo para atributo cuota clase Prestamo.

--- classes/prestamo.py
+++ classes/prestamo.py
@@ -1,21 +1,24 @@
 class Prestamo:
     __auto_incrementoId = 0
-    def __init__(self, fec='', valo=0, num_pag=0, cuo=0, emp='') -> None:
+    def __init__(self, fec='', valo=0, num_pag=3, emp='') -> None:
         self.__id = self.__auto_incrementoId
         self.fecha = fec
         self.valor = valo
         self.numero_pagos = num_pag
-        self.cuota = cuo
-        self.__saldo = self.__calcular_saldo(self.valor, self.numero_pagos, self.cuota)
+        self.__cuota = self.__calcular_cuota(self.valor, self.numero_pagos)
+        self.__saldo = self.__calcular_saldo(self.valor, self.numero_pagos, self.__cuota)
         self.__estado = self.__calcular_estado(self.__saldo)
         self.empleado = emp
         self.__auto_incrementoId += 1
 
     def __str__(self) -> str:
         prestamo_cadena = '(' + self.__id + ', ' + self.fecha + ', ' + str(self.valor) + ', ' + str(self.numero_pagos)\
-                              + ', ' + str(self.cuota) + ', ' + str(self.__saldo) + ', ' + str(self.__estado) + ')'
+                              + ', ' + str(self.__cuota) + ', ' + str(self.__saldo) + ', ' + str(self.__estado) + ')'
         return prestamo_cadena
     
+    def __calcular_cuota(self, valor, numero_pagos) -> float:
+        return round((valor/numero_pagos), 2)
+
     def __calcular_saldo(self, valor, numero_pagos, cuota) -> float:
         if numero_pagos != 0:
             saldo = round(((numero_pagos * cuota) - valor), 2)
@@ -30,13 +33,13 @@ class Prestamo:
             estado = False
         return estado
 
-    def mostrar_empleado(self) -> None:
+    def mostrar_prestamo(self) -> None:
         print('-Clase Prestamo:')
         print(' id:', self.__id)
         print(' fecha:', self.fecha)
         print(' valor:', self.valor)
         print(' numero_pagos:', self.numero_pagos)
-        print(' cuota:', self.cuota)
+        print(' cuota:', self.__cuota)
         print(' saldo:', self.__saldo)
         print(' estado:', self.__estado)
         print(' Empleado():', self.empleado)
