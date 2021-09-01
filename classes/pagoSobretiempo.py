a2bf4f03e449dc2427ebe66947359e0ced8d97f0
Juan Andrade <jpas.juan01@gmail.com>
2021-09-01 16:56:30 -0500

    Metodos de la clase PagoSobretiempo.

--- classes/pagoSobretiempo.py
+++ classes/pagoSobretiempo.py
@@ -8,3 +8,24 @@ class PagoSobretiempo:
         self.__estado = self.__calcular_estado(self.horas_recargo, self.horas_extraordinarias)
         self.empleado = emp
         self.__auto_incrementoId += 1
+
+    def __str__(self) -> str:
+        pagoSobretiempo_cadena = '(' + self.__id + ', ' + self.fecha + ', ' + str(self.horas_recargo) + ', '\
+                              + str(self.horas_extraordinarias) + ', ' + str(self.__estado) + ')'
+        return pagoSobretiempo_cadena
+
+    def __calcular_estado(self, horas_recargo, horas_extraordinarias) -> bool:
+        if horas_recargo != 0 or horas_extraordinarias != 0:
+            estado = True
+        else:
+            estado = False
+        return estado
+
+    def mostrar_empleado(self) -> None:
+        print('-Clase pagoSobretiempo:')
+        print(' id:', self.__id)
+        print(' fecha:', self.fecha)
+        print(' horas_recargo:', self.horas_recargo)
+        print(' horas_extraordinarias:', self.horas_extraordinarias)
+        print(' estado:', self.__estado)
+        print(' Empleado():', self.empleado)
