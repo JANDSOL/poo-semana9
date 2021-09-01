e595563190df785eb3da5925036b3b706c91e91d
Juan Andrade <jpas.juan01@gmail.com>
2021-09-01 17:16:20 -0500

    Metodo de la clase pagoNomina.

--- classes/pagoNomina.py
+++ classes/pagoNomina.py
@@ -9,8 +9,20 @@ class PagoNomina:
         self.antiguedad = ant
         self.prestamo = pre
         self.__total_ingreso = self.__total_ingreso(self.sueldo, self.antiguedad)
-        self.__total_descuento = self.__total_descuento(self.comision, self.iess, self.prestamo.valor)
+        self.__total_descuento = self.__total_descuento(self.comision, self.iess, self.prestamo.cuota)
         self.__liquido_recibir = self.__calculo_liquido(self.__total_ingreso, self.__total_descuento)
         self.empleado = emp
         self.sobretiempo = sobTie
         self.__auto_incrementoId += 1
+
+    def __calculo_iess(self, iess, sueldo) -> float:
+        return round((iess * sueldo), 2)
+    
+    def __total_ingreso(self, sueldo, antiguedad) -> float:
+        return round((sueldo + antiguedad), 2)
+
+    def __total_descuento(self, comision, iess, cuota) -> float:
+        return round((comision + iess + cuota), 2)
+    
+    def __calculo_liquido(self, total_ingreso, total_descuento) -> float:
+        return round((total_ingreso - total_descuento), 2)
